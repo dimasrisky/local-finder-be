37cead6b281999ca35432217c2603271fa013cdb
unknown <rizkyalphazee@gmail.com>
2026-04-19 15:44:54 +0700

    feat: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Local Finder API')
+    .setDescription('Swagger untuk scraping data bisnis via google maps')
+    .setVersion('1.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+
+  app.setGlobalPrefix('v1')
+  
+  SwaggerModule.setup('api/docs', app, document)
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
